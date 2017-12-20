130e07cb717236cd6d39dfe1e1c8f99b22231265
Roymer Centeno <royymer@gmail.com>
2017-12-20 11:13:46 -0400

    Tercer Commit, con la finalidad de corregir errores ortograficos

--- pruebaGo.go
+++ pruebaGo.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main()  {
-  //este es un primer archivos escrito para comprobrar coneccion a Git
+  //este es un primer archivos escrito para comprobrar conexion a Git
   fmt.Println("buenos dias")
 }
