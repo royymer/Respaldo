e1bf36ed49d9539c714cb5dbea7c2044c62482d4
Roymer Centeno <royymer@gmail.com>
2017-12-20 13:54:08 -0400

    Interfaces en Go

---
+++ interfaces.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+import "math"
+
+//asi se declara una interface
+type calculosGeometricos interface{
+  area() float64
+  perimetro() float64
+}
+
+//a continuacion estructura del cuadro
+type cuadrado struct{
+  ancho, alto float64
+}
+
+//a continuacion estructura para cirulos
+ type circulo struct{
+   radio float64
+ }
+
+ //funcion para calcular el area del cuadrado
+ func (s cuadrado) area() float64  {
+   return s.ancho * s.alto
+ }
+
+ //funcion para calcular el perimetro del cuadrado
+
+ func (s cuadrado) perimetro() float64  {
+      return 4 * (s.ancho + s.alto)
+ }
+
+ //funcion para calcular el area de un circulo
+
+ func (c circulo) area() float64  {
+   return math.Pi * c.radio * c.radio
+ }
+
+ //funcion para calcular el perimetro delcirculo
+
+ func (c circulo) perimetro() float64  {
+   return 2 * math.Pi * c.radio
+ }
+
+ //utilizo la interface geometria para los calculosGeometricos
+ func medida(g calculosGeometricos)  {
+   fmt.Println(g)
+   fmt.Println(g.area())
+   fmt.Println(g.perimetro())
+ }
+
+ //funcion principal
+
+ func main()  {
+   s := cuadrado{ancho: 3 , alto: 4}
+   c := circulo{radio : 5}
+
+   medida(s)
+   medida(c)
+ }
