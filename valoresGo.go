98c52fe24e0c9429de6be1f598122a7602f11124
Roymer Centeno <royymer@gmail.com>
2017-12-20 11:37:33 -0400

    Algunos valores admitidos por go

---
+++ valoresGo.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+  fmt.Println("Esto es una" + "Cadena Concatenada")
+  fmt.Println("1+1 =" , 1+1) //Operadores aritmeticos
+  fmt.Println(true && false) //operadores booleanos con AND
+  fmt.Println(true || false) // operadores booleanos con OR
+  fmt.Println(!true) //Negacion
+}
