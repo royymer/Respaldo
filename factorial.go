d8548cd0a4125f5b3c4924ae9778154c1dc3a124
Roymer Centeno <royymer@gmail.com>
2017-12-20 12:39:27 -0400

    Contiene el programa para el factorial de un numero, usando funciones recursivas

---
+++ factorial.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int  {
+  if n == 0 {
+    return 1 //cumpliendo la ley de que el factorial de 0 por definicion es 1
+  }
+  return n * factorial(n - 1) //si es distinto de 0 ejecuta la funcion de factorial
+}
+
+func main()  {
+  fmt.Println(factorial(5))
+}
