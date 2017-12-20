6e65122f6ddad86ed48a497d914cec37134bded9
Roymer Centeno <royymer@gmail.com>
2017-12-20 11:50:25 -0400

    Trabanado con variables en GO

---
+++ variablesGo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+
+func main()  {
+  var ab string = "Primer Valor" //asingno valor a la variable
+  fmt.Println(ab) //imprimo la variable
+
+  var a, b int = 1 , 2 //declaro las variables y asigno los valores
+  fmt.Println(a,b) //imprimo en el orden que asigne y declare
+
+  var t = true //implicitamente declaro que es del tipo booleano
+  fmt.Println(t) //imprimo su respectivo valores
+
+ f := "Esta es una cadena sencilla"
+ fmt.Println(f)
+
+}
