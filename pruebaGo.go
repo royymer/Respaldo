44ae9432db3af91efa3e158363050cca2fff9181
Roymer Centeno <royymer@gmail.com>
2017-12-20 11:18:26 -0400

    Cuarto Commit se agrego otro PrintLn

--- pruebaGo.go
+++ pruebaGo.go
@@ -5,4 +5,5 @@ import "fmt"
 func main()  {
   //este es un primer archivos escrito para comprobrar conexion a Git
   fmt.Println("buenos dias")
+  fmt.PrintLn("Este ejemplo es para demostrar el poder de GIT")
 }
